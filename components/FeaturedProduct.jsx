831a3001a304f88aacc6b9724b84babd94562dbe
aditya95-pixel <sphs.am6@gmail.com>
2025-06-18 04:45:29 +0530

    Update

--- components/FeaturedProduct.jsx
+++ components/FeaturedProduct.jsx
@@ -31,15 +31,15 @@ const FeaturedProduct = () => {
         <div className="w-28 h-0.5 bg-orange-600 mt-2"></div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-14 mt-12 md:px-14 px-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-14 mt-12 md:px-14 px-4 ">
         {products.map(({ id, image, title, description }) => (
           <div key={id} className="relative group">
             <Image
               src={image}
               alt={title}
-              className="group-hover:brightness-75 transition duration-300 w-full h-auto object-cover"
+              className="group-hover:brightness-75 transition duration-300 w-full h-auto object-cover rounded-[5px]"
             />
-            <div className="group-hover:-translate-y-4 transition duration-300 absolute bottom-8 left-8 text-white space-y-2">
+            <div className="group-hover:-translate-y-4 transition duration-300 absolute bottom-8 left-8 text-white space-y-2 rounded-[5px]">
               <p className="font-medium text-xl lg:text-2xl">{title}</p>
               <p className="text-sm lg:text-base leading-5 max-w-60">
                 {description}
